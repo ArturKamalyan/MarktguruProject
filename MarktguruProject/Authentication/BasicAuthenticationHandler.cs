777fb95e6fb69d5669fc9d9c3c43bd021db1d559
Artur Kamalyan <arturkamalyan97@gmail.com>
2023-04-21 15:22:50 +0400

    added basic authentication with hard coded username and password

---
+++ MarktguruProject/Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text;
+
+namespace MarktguruProject.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            }
+
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+
+            if (authHeader.Scheme != "Basic")
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Scheme");
+            }
+
+            if (String.IsNullOrEmpty(authHeader.Parameter))
+                return AuthenticateResult.Fail("Invalid Authorization Parameter");
+
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+            var username = credentials[0];
+            var password = credentials[1];
+
+            if (username == "superadmin" && password == "12345")
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, username) };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                return AuthenticateResult.Success(ticket);
+            }
+            else
+            {
+                return AuthenticateResult.Fail("Invalid Username or Password");
+            }
+        }
+    }
+}
